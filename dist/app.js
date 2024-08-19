5b12dab17a6bac716b2a7d1837dc50b37bd978af
MikeLautensack <mikelautensack100@gmail.com>
2024-08-19 02:29:58 -0400

    trying to get the api working on azure x5

---
+++ dist/app.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// import puppeteer from "puppeteer";
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8080;
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+// app.post("/generate-pdf", async (req, res) => {
+//   console.log("gen pdf hit...");
+//   try {
+//     const { html } = req.body;
+//     if (!html) {
+//       return res.status(400).json({ error: "HTML content is required" });
+//     }
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.setContent(html);
+//     const pdf = await page.pdf({ format: "A4", printBackground: true });
+//     await browser.close();
+//     res.contentType("application/pdf");
+//     res.setHeader("Content-Length", pdf.length);
+//     res.end(pdf, "binary");
+//   } catch (error) {
+//     console.log("Error generating PDF:", error);
+//     res.status(500).json({ error: "Failed to generate PDF" });
+//   }
+// });
+app.listen(port, () => {
+    console.log(`Server running at ${port}`);
+});
