8b2d93be2e2de602841703318ad092e61b381cb9
MikeLautensack <mikelautensack100@gmail.com>
2024-08-19 02:52:39 -0400

    forgot to save lol

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-// import puppeteer from "puppeteer";
+import puppeteer from "puppeteer";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -10,32 +10,32 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// app.post("/generate-pdf", async (req, res) => {
-//   console.log("gen pdf hit...");
-//   try {
-//     const { html } = req.body;
+app.post("/generate-pdf", async (req, res) => {
+  console.log("gen pdf hit...");
+  try {
+    const { html } = req.body;
 
-//     if (!html) {
-//       return res.status(400).json({ error: "HTML content is required" });
-//     }
+    if (!html) {
+      return res.status(400).json({ error: "HTML content is required" });
+    }
 
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-//     await page.setContent(html);
+    await page.setContent(html);
 
-//     const pdf = await page.pdf({ format: "A4", printBackground: true });
+    const pdf = await page.pdf({ format: "A4", printBackground: true });
 
-//     await browser.close();
+    await browser.close();
 
-//     res.contentType("application/pdf");
-//     res.setHeader("Content-Length", pdf.length);
-//     res.end(pdf, "binary");
-//   } catch (error) {
-//     console.log("Error generating PDF:", error);
-//     res.status(500).json({ error: "Failed to generate PDF" });
-//   }
-// });
+    res.contentType("application/pdf");
+    res.setHeader("Content-Length", pdf.length);
+    res.end(pdf, "binary");
+  } catch (error) {
+    console.log("Error generating PDF:", error);
+    res.status(500).json({ error: "Failed to generate PDF" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
