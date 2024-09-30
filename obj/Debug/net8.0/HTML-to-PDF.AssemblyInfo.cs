362f04eee4d3746953252e9b235499d19047d769
MikeLautensack <mikelautensack100@gmail.com>
2024-09-30 15:57:41 -0400

    Endpoint is now properly generating pdfs with a custom filename

--- obj/Debug/net8.0/HTML-to-PDF.AssemblyInfo.cs
+++ obj/Debug/net8.0/HTML-to-PDF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HTML-to-PDF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52616bcaa6d5b7941876cb72ace8aae7555d2b15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed57f40d0c4a4e2af7de81d930b18b4b235090d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HTML-to-PDF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HTML-to-PDF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
